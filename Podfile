9c61df529d0fe42f2ec056ac91575a1e0da96bac
Piotr Sochalewski <piotr.sochalewski@netguru.co>
2018-01-03 14:49:06 +0100

    Updated GRDB.swift, changed .gitignore

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 target 'netguru' do
   use_frameworks!
 
-  pod 'GRDB.swift'
+  pod 'GRDB.swift', '~> 2.4.1'
 
 end
--- Pods/Target Support Files/Pods-netguru/Pods-netguru-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_netguru : NSObject
-@end
-@implementation PodsDummy_Pods_netguru
-@end
